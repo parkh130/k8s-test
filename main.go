f0a6d7bd37a999f4acf7826a9c5f850206975423
hyeonhyeong.park <hyeonhyeong.park@hyeonkyeongparknoMacBook-Pro.local>
2022-11-29 17:11:16 +0900

    set up

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello world")
 }
